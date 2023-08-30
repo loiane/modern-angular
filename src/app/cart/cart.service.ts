a09f777ad408706ada0415d2cc4426698f71c3a1
Loiane <loianeg@gmail.com>
2023-08-30 14:21:57 -0400

    fixed errors

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,5 +1,4 @@
 import { computed, Injectable, signal } from '@angular/core';
-import { BehaviorSubject, combineLatest, map } from 'rxjs';
 
 import { Product } from '../products/product';
 import { CartItem } from './cart-item';
@@ -10,7 +9,6 @@ import { CartItem } from './cart-item';
 export class CartService {
 
   cartItems = signal<CartItem[]>([]);
-  private cartItems$ = new BehaviorSubject<CartItem[]>(this.cartItems());
 
   cartCount = computed(() => this.cartItems().reduce((acc, curr) => acc + curr.quantity, 0));
 
@@ -22,18 +20,18 @@ export class CartService {
   cartTotal = computed(() => this.cartSubTotal() + this.cartTax());
 
   addProduct(product: Product): void {
-    const itemFound = this.cartItems().find((p) => p.product.id === product.id);
-    if (itemFound) {
-      itemFound.quantity += 1;
+    const indexFound = this.cartItems().findIndex((p) => p.product.id === product.id);
+    if (indexFound >= 0) {
+      this.cartItems.mutate((items) => items[indexFound].quantity += 1);
     } else {
       this.cartItems.mutate((items) => items.push({ product, quantity: 1 }));
     }
   }
 
   updateCartQuantity(cartItem: CartItem): void {
-    const itemFound = this.cartItems().find((p) => p.product.id === cartItem.product.id);
-    if (itemFound) {
-      itemFound.quantity = cartItem.quantity;
+    const indexFound = this.cartItems().findIndex((p) => p.product.id === cartItem.product.id);
+    if (indexFound >= 0) {
+      this.cartItems.mutate((items) => items[indexFound] = cartItem);
     }
   }
 
