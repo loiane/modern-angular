ba93678eac522685847e732ea3a5078efb5f4d79
Loiane <loianeg@gmail.com>
2025-05-28 20:05:44 -0400

    refactor: simplify route definition for redirect to products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/products',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: '/products', pathMatch: 'full'  },
   {
     path: 'products',
     loadComponent: () => import('./products/product-list/product-list').then(m => m.ProductList)
