c2ac292627de66cc03980888da8bed2051e4153f
Loiane <loianeg@gmail.com>
2025-11-29 12:09:06 -0500

    chore(deps): update dependencies to Angular v 21.0.1 and remove jest
    * Upgrade Angular dependencies to version 21.0.1
    * Replace Jest with Vitest for testing
    * Remove zoneless setup for Angular testing
    * Update styles to include Angular Material theming
    * Clean up TypeScript configuration files
    * Modify index.html to remove unnecessary classes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, ErrorHandler } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { GlobalErrorHandler } from './shared/services/global-error-handler.service';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideAnimations(),
     provideHttpClient(),
-    { provide: ErrorHandler, useClass: GlobalErrorHandler }
   ]
 };
