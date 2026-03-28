f198128d8f2299d16e99e459d679def81fbcec04
Loiane <loianeg@gmail.com>
2026-03-28 14:43:15 -0400

    feat(products): enhance product card with dynamic data
    * Bind product name, description, and price to the product card.
    * Update button label to be dynamic.
    * Remove unused product list demo from products grid.

--- src/app/products/product-card/product-card.ts
+++ src/app/products/product-card/product-card.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-card',
@@ -10,4 +11,6 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class ProductCard {
 
+  readonly product = input.required<Product>();
+  readonly addButtonLabel = input('Add to Cart');
 }
